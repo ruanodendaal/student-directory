c4346243b706a27e89e7fc7aeb64c6b635b744b8
Ruan Odendaal <ruanodendaal@gmail.com>
2017-02-27 12:36:27 +0000

    Getting input from the user instead of hardcoding students

--- directory.rb
+++ directory.rb
@@ -1,17 +1,19 @@
-# lets put all students into an array
-students = [
-  {name: "Dr. Hanibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffery Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november},
-]
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # create an empty array
+  students = []
+  name = gets.chomp
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+    # add the student hash to the array
+    students << {name: name, cohort: :november}
+    # gets another name from the user
+    name = gets.chomp
+  end
+  # return the array of students
+  students
+end
 
 def print_header
   puts "The students of Villains Academy"
@@ -26,6 +28,7 @@ def print_footer(students)
     puts "Overall, we have #{students.count} great students"
 end
 
+students = input_students
 print_header
 print(students)
 print_footer(students)
