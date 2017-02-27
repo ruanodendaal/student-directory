66446d574ae0b4e1f9ff73b69865f9cc981edf03
Ruan Odendaal <ruanodendaal@gmail.com>
2017-02-27 10:49:58 +0000

    Removed repetition

--- directory.rb
+++ directory.rb
@@ -1,17 +1,20 @@
-student_count = 11
-# first we print the list of students
+# lets put all students into an array
+students = [
+  "Dr. Hanibal Lecter",
+  "Darth Vader",
+  "Nurse Ratched",
+  "Michael Corleone",
+  "Alex DeLarge",
+  "The Wicked Witch of the West",
+  "Terminator",
+  "Freddy Krueger",
+  "The Joker",
+  "Joffery Baratheon",
+  "Norman Bates"
+]
+# and then print them
 puts "The students of Villains Academy"
 puts "------------"
-puts "Dr. Hanibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffery Baratheon"
-puts "Norman Bates"
+students.each { |student| puts student }
 # finally, we print the total number of students
-puts "Overall, we have #{student_count} great students"
+puts "Overall, we have #{students.count} great students"
