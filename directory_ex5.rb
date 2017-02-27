695b73e44b74bc792f90c27d6de91a16a838464a
Ruan Odendaal <ruanodendaal@gmail.com>
2017-02-27 17:20:21 +0000

    Added some more student information

---
+++ directory_ex5.rb
@@ -0,0 +1,47 @@
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # create an empty array
+  students = []
+  name = gets.chomp
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+    # add the student hash to the array
+    #students << {name: name, cohort: :november}
+    puts "What's their favourite hobbie?"
+    hobby = gets.chomp
+    puts "Where were they born?"
+    birth_country = gets.chomp
+    puts "How tall are they? (in cm)"
+    height = gets.chomp
+    students << {name: name, cohort: :november, hobby: hobby, birth_country: birth_country, height: height}
+    # gets another name from the user
+    puts "Next person:"
+    name = gets.chomp
+  end
+
+  # return the array of students
+  students
+end
+
+def print_header
+  puts "The students of Villains Academy"
+  puts "------------"
+end
+
+def print(students)
+  students.each_with_index { |student, i|
+    puts "#{i+1}. #{student[:name]} likes #{student[:hobby]}"
+    puts "was born in #{student[:birth_country]}"
+    puts "and is #{student[:height]}cm tall (#{student[:cohort]} cohort)"
+  }
+end
+
+def print_footer(students)
+    puts "Overall, we have #{students.count} great students"
+end
+
+students = input_students
+print_header
+print(students)
+print_footer(students)
