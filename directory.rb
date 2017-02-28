691429df47af11882f7b9ed265c3612779ba2c75
Ruan Odendaal <ruanodendaal@gmail.com>
2017-02-28 12:54:53 +0000

    added an interactive menu

--- directory.rb
+++ directory.rb
@@ -1,3 +1,28 @@
+def interactive_menu
+  students = []
+  loop do
+    # 1. print the menu and ask the user what to do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+    # 2. read the input and save it into a variable
+    selection = gets.chomp
+    # 3. do what the user has asked
+    case selection
+    when "1"
+      students = input_students
+    when "2"
+      print_header
+      print(students)
+      print_footer(students)
+    when "9"
+      exit # will cause program to terminate
+    else
+      puts "I don't know what you mean, try again"
+    end
+  end
+end
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
@@ -8,6 +33,7 @@ def input_students
   while !name.empty? do
     # add the student hash to the array
     students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
     # gets another name from the user
     name = gets.chomp
   end
@@ -16,8 +42,8 @@ def input_students
 end
 
 def print_header
-  puts "The students of Villains Academy"
-  puts "------------"
+  puts "The students of Villains Academy".center(50)
+  puts "------------".center(50)
 end
 
 def print(students)
@@ -25,10 +51,15 @@ def print(students)
 end
 
 def print_footer(students)
-    puts "Overall, we have #{students.count} great students"
+  puts "------------".center(50)
+  if students.count > 1
+    puts "Overall, we have #{students.count} great students".center(50)
+  else
+    puts "Overall, we have #{students.count} great student".center(50)
+  end
 end
 
-students = input_students
+students = interactive_menu
 print_header
 print(students)
 print_footer(students)
