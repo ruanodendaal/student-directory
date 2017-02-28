4ea9481967c40a71151e954c038b0938bf73c170
Ruan Odendaal <ruanodendaal@gmail.com>
2017-02-28 13:48:26 +0000

    Included ability to save to csv file

--- directory.rb
+++ directory.rb
@@ -12,6 +12,7 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit"
 end
 
@@ -29,6 +30,8 @@ def process(selection)
     input_students
   when "2"
     show_students
+  when "3"
+    save_students
   when "9"
     exit # will cause program to terminate
   else
@@ -70,6 +73,19 @@ def print_footer
   else
     puts "Overall, we have #{@students.count} great student".center(50)
   end
+end
+
+
+def save_students
+  # open file for writing
+  file = File.open("students.csv", "w")
+  # iterate over the array of students & save
+  @students.each { |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  }
+  file.close
 end
 
 interactive_menu
